bde3d874f8350cfd2dc02a04871cbed1c81b2048
Danny Weitekamp <dannyweitekamp@gmail.com>
2025-06-08 18:44:04 -0400

    add a test that nearly aligns with figure

---
+++ tests/test_basic.py
@@ -0,0 +1,39 @@
+from stand.stand import STANDClassifier
+import numpy as np
+
+np.set_printoptions(precision=3)
+
+stand = STANDClassifier(split_choice="all_near_max")
+
+
+# Xy = np.array([
+#    # 1, 2, 3, 4, 5, 6, 7, y
+#     [0, 0, 1, 1, 0, 1, 1, 0], #0
+#     [1, 0, 0, 0, 1, 0, 1, 1], #1
+#     [1, 1, 0, 1, 1, 1, 1, 1], #2
+#     [1, 0, 1, 0, 0, 0, 0, 1], #3
+#     [1, 1, 1, 1, 1, 1, 1, 1], #4
+#     [1, 0, 0, 1, 0, 1, 1, 0], #5
+#     [1, 1, 0, 1, 0, 1, 1, 0], #6
+#     # [1, 1, 0, 1, 0, 1, 1, 0], #7
+# ], dtype=np.int32)
+
+Xy = np.array([
+   # 1, 2, 3, 4, 5, 6, 7, y
+    [0, 1, 1, 1, 0, 1, 1, 0], #0 *
+    [0, 0, 0, 1, 0, 1, 0, 0], #1 *
+    [1, 0, 1, 1, 1, 1, 1, 1], #2 *
+    [0, 1, 0, 1, 1, 1, 0, 1], #3 *
+    [1, 0, 1, 0, 0, 0, 1, 1], #4 
+    [0, 0, 0, 1, 0, 1, 1, 0], #5 *
+    [0, 0, 1, 0, 0, 0, 0, 1], #6 
+    [0, 0, 0, 1, 0, 1, 1, 0], #7 *
+    [0, 0, 1, 0, 0, 0, 1, 1], #8 
+], dtype=np.int32)
+
+X,y = Xy[:,:-1], Xy[:,-1]
+# y = np.array([0, 1, 1, 1, 1, 0, 0, 0], dtype=np.int32)
+
+stand.fit(X,None,y)
+
+print(stand.__str__(leaf_inds=True, node_inds=True))
