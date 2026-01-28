ece5ff77e69c6f276f0e0bf3e6ad8922ccbb986c
Danny Weitekamp <dannyweitekamp@gmail.com>
2026-01-28 17:42:52 -0500

    tweaking data

--- tests/stand_figure_case.py
+++ tests/stand_figure_case.py
@@ -2,7 +2,12 @@ from stand.stand import STANDClassifier
 from stand.tree_classifier import SeqCovClassifier, TreeClassifier
 import numpy as np
 
+def inv_mapper(inp_key, inp_val):
+   return False, int(inp_key) + 1, inp_val
+
+
 dt = TreeClassifier(
+   inv_mapper=inv_mapper
 )
 
 stand = STANDClassifier(
@@ -10,6 +15,7 @@ stand = STANDClassifier(
    # lam_e=25.0,
    slip=.5,
    impurity_func="gini",
+   inv_mapper=inv_mapper,
    # fit_method="sequential_cover",
    split_choice="all_near_max",
    w_path_slip=True
@@ -19,15 +25,23 @@ stand = STANDClassifier(
 Xy = np.array([
    # 1, 2, 3, 4, 5, 6, 7, y
     [0, 0, 1, 1, 0, 1, 1, 0], #0
-    [1, 0, 0, 0, 1, 0, 1, 1], #1
+    [1, 1, 1, 0, 1, 0, 1, 1], #1
     [1, 1, 0, 1, 1, 1, 1, 1], #2
-    [1, 0, 1, 0, 0, 0, 0, 1], #3
+    [1, 0, 0, 0, 0, 0, 0, 1], #3
     [1, 1, 1, 1, 1, 1, 1, 1], #4
-    [1, 0, 0, 1, 0, 1, 1, 0], #5
-    [1, 1, 0, 1, 0, 1, 1, 0], #6
+    [1, 0, 1, 1, 0, 1, 1, 0], #5
+    [1, 1, 1, 1, 0, 0, 1, 0], #6
     # [1, 1, 0, 1, 0, 1, 1, 0], #7
 ], dtype=np.int32)
 
+
+for i,x0 in enumerate(Xy[:1]):
+   for j,x1 in enumerate(Xy[:1]):
+      if(i == j): 
+         continue
+      assert(np.array_equal(x1,x2))
+
+   
 
 X,y = Xy[:,:-1], Xy[:,-1]
 # y = np.array([0, 1, 1, 1, 1, 0, 0, 0], dtype=np.int32)
@@ -38,6 +52,10 @@ X,y = Xy[:,:-1], Xy[:,-1]
 # print(seq_cov.__str__(leaf_inds=True, node_inds=True))
 
 # raise ValueError()
+
+
+
+
 
 dt.fit(X,None,y)
 stand.fit(X,None,y)
