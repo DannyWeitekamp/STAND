92d6cc3dbff7b1d7086520ff0538717e353a3a8a
Danny Weitekamp <dannyweitekamp@gmail.com>
2026-02-06 10:48:14 -0500

    added stand figure case

--- tests/stand_figure_case.py
+++ tests/stand_figure_case.py
@@ -12,7 +12,7 @@ dt = TreeClassifier(
 )
 
 stand = STANDClassifier(
-   # lam_p=25.0,
+   lam_p=25.0,
    # lam_e=25.0,
    slip=.5,
    impurity_func="gini",
@@ -50,26 +50,56 @@ stand = STANDClassifier(
 
 Xy = np.array([
    # 1, 2, 3, 4, 5, 6, 7, y
-    [0, 0, 1, 1, 0, 1, 1, 0], #0
-    [1, 1, 1, 0, 1, 0, 1, 1], #1
-    [1, 1, 0, 1, 1, 1, 1, 1], #2
-    [1, 0, 1, 0, 0, 0, 0, 1], #3
+    [0, 1, 1, 0, 1, 1, 0, 0], #0
+    [1, 1, 0, 1, 0, 1, 1, 1], #1
+    [1, 0, 1, 1, 1, 1, 1, 1], #2
+    [1, 1, 0, 0, 0, 0, 0, 1], #3
     [1, 1, 1, 1, 1, 1, 1, 1], #4
-    [1, 0, 1, 1, 0, 1, 1, 0], #5
-    [1, 1, 1, 1, 0, 1, 1, 0], #6
-    [1, 1, 1, 1, 0, 0, 1, 0], #7
+    [1, 1, 1, 0, 1, 1, 0, 0], #5
+    [1, 1, 1, 0, 1, 1, 1, 0], #6
+    # [1, 1, 1, 1, 1, 0, 0, 0], #7
+    # [1, 1, 1, 1, 0, 1, 1, 1], #8
 ], dtype=np.int32)
 
 
-for i,x0 in enumerate(Xy[:1]):
-   for j,x1 in enumerate(Xy[:1]):
-      if(i == j): 
-         continue
-      assert(np.array_equal(x1,x2))
+
+
+def print_specific_ex(Xy, inds):
+   sub = Xy[inds][:,:-1]
+   if(len(inds) == 1):
+      all_same = np.ones(len(sub[0]))
+   else:
+      all_same = sub[0] == sub[1]
+   for x in sub[2:]:
+      all_same &= (x == sub[0])
+   
+   arr = []
+   for i, x in enumerate(all_same):
+      if(x):
+         if(sub[0,i] == 1):
+            arr.append(f"X{i+1}")
+         else:
+            arr.append(f"¬X{i+1}")
+   return f"specific ext:{inds}: {', '.join(arr)}"
+
+print(print_specific_ex(Xy, [3]))
+print(print_specific_ex(Xy, [0,5,6]))
+print(print_specific_ex(Xy, [1,3]))
+print(print_specific_ex(Xy, [1,2,4]))
+print(print_specific_ex(Xy, [0,5,6]))
+print(print_specific_ex(Xy, [2,4]))
+print(print_specific_ex(Xy, [6]))
+
 
    
 
 X,y = Xy[:,:-1], Xy[:,-1]
+
+for i,x0 in enumerate(X):
+   for j,x1 in enumerate(X):
+      if(i == j): 
+         continue
+      assert(np.array_equal(x0,x1), f"x0: {x0}, x1: {x1}")
 # y = np.array([0, 1, 1, 1, 1, 0, 0, 0], dtype=np.int32)
 
 
@@ -79,18 +109,59 @@ X,y = Xy[:,:-1], Xy[:,-1]
 
 # raise ValueError()
 
+import re
+def easy_print(s):
 
+   pf_map = {}
+   for i in range(11):
+      try:
+         match = re.search(r'(NODE|ROOT|LEAF)\(' +str(i)+'\)', s)
+         print(match, match.group(0), match.group(1), i)
+         pf_map[str(i)] = match.group(1)[0]
+      except:
+         pass
 
+   print(pf_map)
 
+   matches = re.finditer(r'\(\[(\d+)\]==(\d+)\)\[F:(\d+) T:(\d+)\]', s)
+   for match in matches:
+      x = match.group(0)
+      ft = match.group(1)
+      val = match.group(2)
+      false_dest = match.group(3)
+      true_dest = match.group(4)
+      l_dest, r_dest = (false_dest, true_dest) if val=="1" else (true_dest, false_dest)
+      l_dest, r_dest = f"{pf_map[l_dest]}{l_dest}", f"{pf_map[r_dest]}{r_dest}"
+      s = s.replace(x, f"¬X{ft}→ {l_dest}, X{ft}→ {r_dest}")
+      # print(f"-X{ft} -> {l_dest}, -X{ft} -> {r_dest}")
+
+   return s
+
+def easy_print_opts(s):
+   matches = re.finditer(r'(~*)\((\d+)==(\d+)\)', s)
+   for match in matches:
+      x = match.group(0)
+      neg = match.group(1)
+      ft = int(match.group(2)) +1
+      val = match.group(3)
+      invert = len(neg) > 0 ^ (val == "0")
+      if(invert):
+         s = s.replace(x, f"¬X{ft}")
+      else:
+         s = s.replace(x, f"X{ft}")
+   return s
 
 dt.fit(X,None,y)
 stand.fit(X,None,y)
 
 print("------  STAND  -------")
 print(stand.__str__(leaf_inds=True, node_inds=True))
+print(easy_print(stand.__str__(leaf_inds=True, node_inds=True)))
 opt_conjs = stand.get_opt_conjs_for_label(1)
-print(opt_conjs_str(stand.op_tree, opt_conjs))
+print(easy_print_opts(opt_conjs_str(stand.op_tree, opt_conjs)))
 
 print("-------  DT  --------")
 print(dt.__str__(leaf_inds=True, node_inds=True))
-
+print(easy_print(dt.__str__(leaf_inds=True, node_inds=True)))
+opt_conjs = dt.get_opt_conjs_for_label(1)
+print(opt_conjs_str(dt.tree, opt_conjs))
